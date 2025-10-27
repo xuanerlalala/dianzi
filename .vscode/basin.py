23ec3278f32a766894743455f9a23b8463b93e8c
xuanerlalala <hzxuaner@outlook.com>
2025-10-27 14:32:37 +0800

    更新了hello.py

--- .vscode/basin.py
+++ .vscode/basin.py
@@ -1,2 +1,10 @@
 print("Hello, world!")
-print("Python", "is", "fun", sep="-", end="!\n")
+print("Python", "is", "fun!!", sep="-", end="!\n")
+a=True
+print(type(a))
+x,y=10,3
+print(x**y)
+name=input("please input your name")
+age=int(input("please input your year"))
+print(f"{name}hello,you will{age+1}years old next year!")
+
